bda37fb834e1df3f3871b30426f167a26bef4753
sgk0713 <sgk0713@gmail.com>
2019-03-07 00:34:54 +0900

    update

---
+++ src/algolani/B_16235.java
@@ -0,0 +1,121 @@
+package algolani;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B_16235 {
+	int N, M, K;
+	int[][] energyMap, map;
+	int[] dirR = {0, 1, 0, -1, 1, 1, -1, -1};
+	int[] dirC = {1, 0, -1, 0, 1, -1, 1, -1};
+	PriorityQueue<Tree_16235> q = new PriorityQueue<>();
+	Queue<Tree_16235> deadTree = new LinkedList<>();
+	Queue<Tree_16235> makeOcta = new LinkedList<>();
+	public B_16235() {//39분
+try {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken())+1;
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		energyMap = new int[N][N];
+		map = new int[N][N];
+		for(int i= 1;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 1;j<N;j++) {
+				map[i][j] = 5;
+				energyMap[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			int age = Integer.parseInt(st.nextToken());
+			q.add(new Tree_16235(r, c, age, 0));
+		}
+		int year = 0;
+		while(true) {
+			year++;
+			int qSize = q.size();
+			//spring
+			for(int t = 0;t<qSize;t++) {
+				Tree_16235 tree = q.poll();
+				int r = tree.r;
+				int c = tree.c;
+				int age = tree.age;
+				if(map[r][c]>=age) {
+					map[r][c] -= age;
+					tree.age++;
+					tree.year = year;
+					if(tree.age%5==0) {
+						makeOcta.offer(tree);
+					}
+					q.offer(tree);
+				}else {
+					tree.age /= 2;
+					deadTree.offer(tree);
+				}
+			}
+			//summer
+			while(!deadTree.isEmpty()) {
+				Tree_16235 tree = deadTree.poll();
+				map[tree.r][tree.c] += tree.age;
+			}
+			//autumn
+			while(!makeOcta.isEmpty()) {
+				Tree_16235 tree = makeOcta.poll();
+				int r = tree.r;
+				int c = tree.c;
+				for(int i= 0;i<8;i++) {
+					int rr = r + dirR[i];
+					int cc = c + dirC[i];
+					if(isInBoundery(rr, cc)) {
+						q.offer(new Tree_16235(rr, cc, 1, year));
+					}
+				}
+			}
+			//winter
+			if(year<K) {
+				addEnergy();
+			}else {
+				break;
+			}
+		}
+		System.out.println(q.size());
+}catch (Exception e) {}
+	}
+	void addEnergy() {
+		for(int i = 1;i<N;i++) {
+			for(int j =1 ;j<N;j++) {
+				map[i][j]+=energyMap[i][j];
+			}
+		}
+	}
+	boolean isInBoundery(int r, int c){
+		if(r>0 && r<N && c>0 && c<N) {
+			return true;
+		}
+		return false;
+		
+	}
+	class Tree_16235 implements Comparable<Tree_16235>{
+		int r, c, age, year;
+		public Tree_16235(int r, int c, int age, int year) {
+			this.r = r;
+			this.c = c;
+			this.age = age;
+			this.year = year;
+		}
+		@Override
+		public int compareTo(Tree_16235 o) {
+			if(this.year>o.year) return 1;
+			else if(this.year < o.year) return -1;
+			else return this.age - o.age;
+		}
+	}
+}
--- src/algolani/B_16988.java
+++ src/algolani/B_16988.java
@@ -2,6 +2,7 @@ package algolani;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -11,13 +12,10 @@ public class B_16988 {//Baaaaaaaaaduk2 (Easy)
 	boolean[][] isChecked;
 	int[] dirR = {0,1,0,-1};
 	int[] dirC = {1,0,-1,0};
-	int R, C, answer;
-	LinkedList<Pair_16988> groupList = new LinkedList<>();
-	LinkedList<Node_16988> nodeList = new LinkedList<>();
+	int[][] stones;
+	int R, C, answer, add;
+	ArrayList<Node_16988> oneList = new ArrayList<>();
 	Queue<Node_16988> q = new LinkedList<>();
-	Node_16988 tempFirst = null;
-	Node_16988 tempSecond = null;
-	
 	public B_16988() {
 try {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,6 +24,7 @@ try {
 		C = Integer.parseInt(st.nextToken());
 		map = new int[R+2][C+2];
 		isChecked = new boolean[R+2][C+2];
+		stones = new int[(R+2)*(C+2)][(R+2)*(C+2)];
 		for(int i = 0;i<R+2;i++) {
 			if(i != 0 && i!=R+1) {
 				st = new StringTokenizer(br.readLine());
@@ -38,142 +37,106 @@ try {
 				}
 			}
 		}
-		int groupNum = 0;
-		for(int i = 1;i<R+1;i++) {
-			for(int j = 1;j<C+1;j++) {
+		for(int i = 1;i<R+2;i++) {
+			for(int j = 1;j<C+2;j++) {
 				if(isChecked[i][j] == false && map[i][j] == 2) {
-					q.offer(new Node_16988(i, j));
-					isChecked[i][j] = true;
 					int count = 1;
-					tempFirst = null;
-					tempSecond = null;
-					boolean isPossible = true;
+					isChecked[i][j] = true;
+					q.offer(new Node_16988(i, j));
+					int first = 0;
+					int second = 0;
+					boolean isOverLimit = false;
 					while(!q.isEmpty()) {
-						Node_16988 tempNode = q.poll();
-						int r = tempNode.r;
-						int c = tempNode.c;
+						Node_16988 temp = q.poll();
+						int r = temp.r;
+						int c = temp.c;
 						for(int k = 0;k<4;k++) {
-							int rr = r+dirR[k];
-							int cc = c+dirC[k];
-							if(isChecked[rr][cc] == false) {
+							int rr = r + dirR[k];
+							int cc = c + dirC[k];
+							if(map[rr][cc]==2 && isChecked[rr][cc] == false) {
+								count++;
 								isChecked[rr][cc] = true;
-								
-								if(map[rr][cc] == 1) {
-									continue;
-								}else if(map[rr][cc] == 0 && isPossible) {
-									if(groupList.size()==groupNum) {
-										tempFirst = new Node_16988(rr, cc);
-										groupList.add(new Pair_16988(tempFirst, null, count));
-									}else {
-										if(groupList.get(groupNum).second == null) {
-											tempSecond = new Node_16988(rr, cc);
-											groupList.get(groupNum).second = tempSecond;
-										}else {
-											groupList.removeLast();
-											isPossible = false;
-										}
-									}
-								}else if(map[rr][cc] == 2) {
-									q.offer(new Node_16988(rr, cc));
-									count++;
+								q.offer(new Node_16988(rr, cc));
+							}else if(map[rr][cc] == 0 && isChecked[rr][cc] == false) {
+								if(first == 0) {
+									first = rr*(C+2)+cc;
+									isChecked[rr][cc] = true;
+								}else if(second == 0) {
+									second = rr*(C+2)+cc;
+									isChecked[rr][cc] = true;
+								}else {
+									isOverLimit = true;
 								}
 							}
 						}
 					}
-					if(isPossible) {
-						boolean isFisrtPossible = true, isSecondPossible = true;
-						
-						for(Node_16988 temp : nodeList) {
-							if(temp.equals(tempFirst)) {
-								isFisrtPossible = false;
-							}
-							if(temp.equals(tempSecond)) {
-								isSecondPossible = false;
-							}
-						}
-						if(tempFirst != null && isFisrtPossible == true) {
-							nodeList.add(tempFirst);
-						}
-						if(tempSecond != null && isSecondPossible == true) {
-							nodeList.add(tempSecond);
-						}
-						groupList.getLast().count = count;
-						groupNum++;
+					if(first != 0) {
+						isChecked[first/(C+2)][first-((first/(C+2))*(C+2))] = false;
 					}
-					if(tempFirst != null) {
-						isChecked[tempFirst.r][tempFirst.c] = false;
+					if(second != 0) {
+						isChecked[second/(C+2)][second-((second/(C+2))*(C+2))] = false;
 					}
-					if(tempSecond != null) {
-						isChecked[tempSecond.r][tempSecond.c] = false;
+                    if(!isOverLimit && first != 0) {
+						if(second == 0) {
+							boolean isThere = false;
+							for(Node_16988 node: oneList) {
+								if(node.r == first) {
+									node.count += count;
+									isThere = true;
+									break;
+								}
+							}
+							if(!isThere) {
+								oneList.add(new Node_16988(first, 0, count));
+							}
+						}else {
+							answer = Math.max(answer, stones[first][second] += count);
+							answer = Math.max(answer, stones[second][first] += count);
+						}
 					}
 				}
 			}
 		}
-		int groupSize = groupList.size();
-		int nodeSize = nodeList.size();
-		int tempAnswer = 0;
-		for(int i = 0;i<nodeSize;i++) {
-			tempFirst = nodeList.get(i);
-			for(int j = i+1;j<nodeSize;j++) {
-				tempSecond = nodeList.get(j);
-				
-				tempAnswer = 0;
-				
-				for(int k = 0;k<groupSize;k++) {
-					Pair_16988 tempPair = groupList.get(k);
-					Node_16988 pairFirst = tempPair.first;
-					Node_16988 pairSecond = tempPair.second;
-					if(pairSecond == null) {
-						if(tempFirst.equals(pairFirst) || tempSecond.equals(pairFirst)) {
-							tempAnswer+=tempPair.count;
-						}
-					}else {
-						if(tempFirst.equals(pairFirst)&& tempSecond.equals(pairSecond) || tempFirst.equals(pairSecond)&& tempSecond.equals(pairFirst)) {
-							tempAnswer+=tempPair.count;
-						}
-					}
-				}
-				answer = Math.max(answer, tempAnswer);
-				Pair_16988 temp = groupList.getFirst();
-				groupList.removeFirst();
-				groupList.addLast(temp);
+		int len = oneList.size();
+		for(int i = 0;i<stones.length;i++) {
+			int maxNum = 0;
+			for(int j = 0;j<len;j++) {
+				int first = oneList.get(j).r;
+				int firstCount = oneList.get(j).count;
+				int tempSum = stones[first][i];
+				maxNum = Math.max(maxNum, tempSum + firstCount);
 			}
-			
+			answer = Math.max(maxNum, answer);
 		}
-			
+		for(int i = 0;i<len-1;i++) {
+			int first = oneList.get(i).r;
+			int firstCount = oneList.get(i).count;
+			for(int j = i+1;j<len;j++) {
+				int second = oneList.get(j).r;
+				int secondCount = oneList.get(j).count;
+				answer = Math.max(answer, stones[first][second] += (firstCount+secondCount));
+				answer = Math.max(answer, stones[second][first] += (firstCount+secondCount));
+			}
+		}
 		System.out.println(answer);
-		
 }catch (Exception e) {e.printStackTrace();}
 	}
 	class Node_16988{
-		int r, c;
+		int r, c, count;
 		public Node_16988(int r, int c) {
 			this.r = r;
 			this.c = c;
 		}
+		public Node_16988(int first, int second, int count) {
+			this.r = first;
+			this.c = second;
+			this.count = count;
+		}
 		public boolean equals(Node_16988 obj) {
-			if(obj != null && (r == obj.r && c == obj.c)) {
+			if((obj.r == r&& obj.c == c)||(obj.c == r && obj.r == c)) {
 				return true;
 			}
 			return false;
 		}
-	}
-	class Pair_16988{
-		Node_16988 first, second;
-		int count;
-		public Pair_16988(Node_16988 first, Node_16988 second, int count) {
-			this.first = first;
-			this.second = second;
-			this.count = count;
-		}
-	}
-	void showMap() {
-		for(int i = 0;i<R+2;i++) {
-			for(int j = 0;j<C+2;j++) {
-				System.out.print(map[i][j] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println();
 	}
 }
